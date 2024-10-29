e59d9bd52df6ac60069a3bba19e1dd21e0f04290
Paul Clue <67766160+Paul-Clue@users.noreply.github.com>
2024-10-29 10:01:00 -0500

    Add user and connect clerk and convex users

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,10 +2,15 @@ import { defineSchema, defineTable } from 'convex/server';
 import { v } from 'convex/values';
 
 export default defineSchema({
+  users: defineTable({
+    username: v.string(),
+    image: v.string(),
+    clerkId: v.string(),
+  }).index('by_clerk_id', ['clerkId']),
   messages: defineTable({
     sender: v.string(),
     content: v.string(),
-  })
-    // .searchIndex('search_sender', { searchField: 'sender' })
-    // .searchIndex('search_content', { searchField: 'content' }),
+  }),
+  // .searchIndex('search_sender', { searchField: 'sender' })
+  // .searchIndex('search_content', { searchField: 'content' }),
 });
